7bf2db2ab91f1aa1f8df7c2d055bbb1ab3c9cf94
Stephen McCarthy <stephen.mccarthy@digital.cabinet-office.gov.uk>
2017-11-14 08:46:24 +0000

    added direct debit flows

--- app/routes.js
+++ app/routes.js
@@ -7,5 +7,17 @@ router.get('/', function (req, res) {
 })
 
 // add your routes here
+router.get('/direct-debit-payment', function (req, res) {
+  // get the answer from the query string (eg. ?over18=false)
+  var radiogroup = req.query.radiogroup
+
+  if (radiogroup === 'card-payment') {
+    // redirect to the relevant page
+    res.redirect('/card-payment')
+  } else {
+    // if over18 is any other value (or is missing) render the page requested
+    res.redirect('/direct-debit-payment-1')
+  }
+})
 
 module.exports = router
