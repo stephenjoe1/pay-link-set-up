da3b4cd3ca299a7ed8bf39d537319a1672ef26ff
Stephen McCarthy <stephen.mccarthy@digital.cabinet-office.gov.uk>
2018-02-19 22:12:25 +0000

    added new pages for car tax and council tax

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for a Lock and Weir Fishing Permit',
+  serviceName: 'Tax your vehicle',
 
   // Default port that prototype runs on
   port: '3000',
