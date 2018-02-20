258f94752c3d03f318fe09f3cd27646444934ae6
Stephen McCarthy <stephen.mccarthy@digital.cabinet-office.gov.uk>
2018-02-20 12:25:34 +0000

    added card payment flow for car tax

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
-// Route index page
+// Route index page 
 router.get('/', function (req, res) {
   res.render('index')
 })
@@ -14,10 +14,28 @@ router.get('/pick-plan-type', function (req, res) {
   if (radiogroup === 'direct-debit-payment-monthly-1') {
     // redirect to the relevant page
     res.redirect('/driving-pages/direct-debit-payment-monthly-1')
+  } else if (radiogroup === 'direct-debit-payment-6monthly-1'){
+    // if over18 is any other value (or is missing) render the page requested
+    res.redirect('/driving-pages/direct-debit-payment-6monthly-1')
   } else {
     // if over18 is any other value (or is missing) render the page requested
     res.redirect('/driving-pages/direct-debit-payment-yearly-1')
-  } 
+  }  
+})
+
+
+// add your routes here
+router.get('/pick-payment-type', function (req, res) {
+  // get the answer from the query string (eg. ?over18=false)
+  var radiogroup = req.query.radiogroup
+
+  if (radiogroup === 'renew-car-tax-choose-period') {
+    // redirect to the relevant page
+    res.redirect('/driving-pages/renew-car-tax-choose-period')
+  } else {
+    // if over18 is any other value (or is missing) render the page requested
+    res.redirect('/driving-pages/renew-car-tax-card-summary')
+  }  
 })
 
 
